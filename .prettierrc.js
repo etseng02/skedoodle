4e4a5257c23032401d43270fd078e94fc5f11435
Eddie <eddietseng@outlook.com>
2020-07-17 00:59:23 -0400

    Initial commit, set up prettier, eslint and deleted readme contents

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  endOfLine: 'lf',
+  parser: 'babel',
+  semi: false,
+  semicolons: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+};
